30ce2bac4b6dba67eb7fcc0ffc7182145c3a51bd
Michaeldam88 <michaeldam88@gmail.com>
2022-11-06 13:19:20 +0100

    added the push function

--- js/array-functions.test.js
+++ js/array-functions.test.js
@@ -1,13 +1,28 @@
-import { arrayLength } from './array-functions';
+import { arrayLength, arrayPush } from './array-functions';
 
 describe('Given arrayLength function', () => {
-    const arr = [
+    const arrCase = [
         [[3, 8, 5, 6], 4],
         [[], 0],
         [['1'], 1],
     ];
-    test.each(arr)(`The result of %p should be %i`, (arr, expected) => {
+    test.each(arrCase)(`The result of %p should be %i`, (arr, expected) => {
         const result = arrayLength(arr);
         expect(result).toBe(expected);
     });
+});
+
+describe('Given arrayPush function', () => {
+    const arrCase = [
+        [[3, 8], 5, [3, 8, 5]],
+        [[3, 8, 5], 6, [3, 8, 5, 6]],
+        [[3, 8, 5, 6], 'x', [3, 8, 5, 6, 'x']],
+    ];
+    test.each(arrCase)(
+        `The result of %p + %p should be %p`,
+        (originaArr, newElement, expected) => {
+            const result = arrayPush(originaArr, newElement);
+            expect(result).toStrictEqual(expected);
+        }
+    );
 });
